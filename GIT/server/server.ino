158158582515cf25e6efe28aaac8fa7b3f835ffe
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2022-12-08 01:07:29 +0500

    ignoring_git_help_going_for_yt

--- GIT/server/server.ino
+++ GIT/server/server.ino
@@ -1,12 +1,14 @@
-#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+
 
 // Configuration parameters for Access Point
-char * ssid_ap = "ANY_SSID";
-char * password_ap = "ANY_PASSWORD";
-IPAddress ip(192,168,11,4); // arbitrary IP address (doesn't conflict w/ local network)
-IPAddress gateway(192,168,11,1);
-IPAddress subnet(255,255,255,0);
+char *ssid_ap = "Edge_network";
+char *password_ap = "123456789";
+IPAddress ip(192, 168, 11,
+             4); // arbitrary IP address (doesn't conflict w/ local network)
+IPAddress gateway(192, 168, 11, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 // Set up the server object
 ESP8266WebServer server;
@@ -16,15 +18,19 @@ float sensor_value = 0.0;
 
 void setup() {
   WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(ip,gateway,subnet);
-  WiFi.softAP(ssid_ap,password_ap);
+  WiFi.softAPConfig(ip, gateway, subnet);
+  WiFi.softAP(ssid_ap, password_ap);
   // Print IP Address as a sanity check
   Serial.begin(115200);
   Serial.println();
-  Serial.print("IP Address: "); Serial.println(WiFi.localIP());
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
   // Configure the server's routes
-  server.on("/",handleIndex); // use the top root path to report the last sensor value
-  server.on("/update",handleUpdate); // use this route to update the sensor value
+  server.on(
+      "/",
+      handleIndex); // use the top root path to report the last sensor value
+  server.on("/update",
+            handleUpdate); // use this route to update the sensor value
   server.begin();
 }
 
@@ -34,12 +40,14 @@ void loop() {
 }
 
 void handleIndex() {
-  server.send(200,"text/plain",String(sensor_value)); // we'll need to refresh the page for getting the latest value
+  server.send(200, "text/plain",
+              String(sensor_value)); // we'll need to refresh the page for
+                                     // getting the latest value
 }
 
 void handleUpdate() {
   // The value will be passed as a URL argument
   sensor_value = server.arg("value").toFloat();
   Serial.println(sensor_value);
-  server.send(200,"text/plain","Updated");
+  server.send(200, "text/plain", "Updated");
 }
