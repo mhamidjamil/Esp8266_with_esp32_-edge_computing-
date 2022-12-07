8cd1c974f1f1a5d696cc4e87bd869999f7470dbc
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2022-12-08 00:46:23 +0500

    git_help

--- client/client.ino
+++ client/client.ino
@@ -15,17 +15,6 @@ const char *serverNameHumi = "http://192.168.4.1/humidity";
 const char *serverNamePres = "http://192.168.4.1/pressure";
 const char *serverPatientData = "http://192.168.4.1/data";
 
-// #include <Wire.h>
-// #include <Adafruit_GFX.h>
-// #include <Adafruit_SSD1306.h>
-
-// #define SCREEN_WIDTH 128 // OLED display width, in pixels
-// #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 String temperature;
 String humidity;
 String pressure;
@@ -37,15 +26,6 @@ const long interval = 5000;
 void setup() {
   Serial.begin(115200);
   Serial.println();
-
-  // Address 0x3C for 128x64, you might need to change this value (use an I2C
-  // scanner)
-  //   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-  //     Serial.println(F("SSD1306 allocation failed"));
-  //     for(;;); // Don't proceed, loop forever
-  //   }
-  //   display.clearDisplay();
-  //   display.setTextColor(WHITE);
 
   Serial.print("Connecting to : ");
   Serial.println(ssid);
@@ -95,7 +75,7 @@ String httpGETRequest(const char *serverName) {
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
     payload = http.getString();
-    Serial.println(payload);
+    // Serial.println(payload);
   } else {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
